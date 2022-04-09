80a19b859755580e115eede4152b4547d7170643
danieldburleson <daniel4193@gmail.com>
2022-04-09 14:58:09 -0700

    Added target buffer for memory improvement

--- solution.py
+++ solution.py
@@ -32,11 +32,21 @@ Two random coordinate generator has been provided for testing purposes.
 Manual mode where you can use your mouse as also been added for testing purposes.
 """
 previous_frame = None
-previous_target = [0, 0]
+buffer_length = 5
+previous_targets = [(0, 0)]*buffer_length
 
 def GetLocation(move_type, env, current_frame):
     global previous_frame
     global previous_target
+
+    # Parameters
+    medianBlur_ksize = 9
+    gaussianBlur_ksize = 5
+    erode_ksize = 7
+    dilate_ksize = 30
+    threshhold = 25
+    contour_amin = 900.0
+    contour_amax = 5000.0
     
     #Use relative coordinates to the current position of the "gun", defined as an integer below
     if move_type == "relative":
@@ -71,8 +81,8 @@ def GetLocation(move_type, env, current_frame):
 
         # convert to greyscale and blur
         processed_frame = cv2.cvtColor(current_frame, cv2.COLOR_RGB2GRAY)
-        processed_frame = cv2.medianBlur(src=processed_frame, ksize=9)
-        processed_frame = cv2.GaussianBlur(src=processed_frame, ksize=(5,5), sigmaX=0)
+        processed_frame = cv2.medianBlur(src=processed_frame, ksize=medianBlur_ksize)
+        processed_frame = cv2.GaussianBlur(src=processed_frame, ksize=(gaussianBlur_ksize, gaussianBlur_ksize), sigmaX=0)
         
         # instantiate previous frame at first run
         if previous_frame is None:
@@ -83,19 +93,21 @@ def GetLocation(move_type, env, current_frame):
         previous_frame = processed_frame
 
         # kernel for erosion
-        kernel = np.ones((7,7))
+        erode_kernel = np.ones((erode_ksize, erode_ksize))
+        dilate_kernel = np.ones((dilate_ksize, dilate_ksize))
 
         # apply a thresholding to diff_frame to remove small differences (movements)
-        threshholded_frame = cv2.threshold(src=diff_frame, thresh=150, maxval=255, type=cv2.THRESH_BINARY)[1]
+        threshholded_frame = cv2.threshold(src=diff_frame, thresh=threshhold, maxval=255, type=cv2.THRESH_BINARY)[1]
         
         # erode result to make the resultant 'blobs' of movement smaller
-        threshholded_frame = cv2.erode(src=diff_frame, kernel=kernel)
+        threshholded_frame = cv2.erode(src=threshholded_frame, kernel=erode_kernel)
+        threshholded_frame = cv2.dilate(src=threshholded_frame, kernel=dilate_kernel)
 
         # create contours from the values found from abs_diff
         contours, _ = cv2.findContours(image=threshholded_frame, mode=cv2.RETR_EXTERNAL, method=cv2.CHAIN_APPROX_TC89_L1)
         for contour in contours:
             # if contour is large enough (but not too large), accept as duck
-            if (cv2.contourArea(contour) > 600) and (cv2.contourArea(contour) < 1000):
+            if (cv2.contourArea(contour) > contour_amin and cv2.contourArea(contour) < contour_amax):
                 (x,y), _ = cv2.minEnclosingCircle(contour)
                 center = (int(x), int(y))
                 cv2.circle(overlay_frame, center, radius=15, color=(255, 0, 0), thickness=2)
@@ -112,16 +124,24 @@ def GetLocation(move_type, env, current_frame):
             coordinate = [coordinatex, coordinatey]
         else:
             # try to find a duck that is less likely to be the same duck
-            while len(duck_locations) > 0: 
+            while len(duck_locations) > 0:
+                found_target = True 
                 coordinatey, coordinatex = duck_locations.pop(0)
                 # coordinatey, coordinatex = max(duck_locations, key=lambda item:item[0])
                 coordinate = [coordinatex, coordinatey]
-                if coordinatex not in range(previous_target[0] - 100, previous_target[0] + 100) and len(duck_locations) > 0:
+                for previous_target in previous_targets:                    
+                    if coordinatex in range(previous_target[0] - 100, previous_target[0] + 100):
+                        found_target = False
+                        break
+
+                if found_target == True:
                     break
             
         # Store previous target to make sure the same target isnt hit twice
-        previous_target = coordinate
+        cv2.circle(overlay_frame, (coordinatey, coordinatex), radius=15, color=(0, 0, 255), thickness=2)
+        previous_targets.pop(0)
+        previous_targets.append(coordinate)
 
         # Create debug window
-        visualizer([diff_frame, overlay_frame], nrows=1, ncols=2, windowScale=1.5)
+        visualizer([threshholded_frame, overlay_frame], nrows=1, ncols=2, windowScale=1.5)
     return [{'coordinate' : coordinate, 'move_type' : move_type}]
